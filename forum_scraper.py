59070fd06d77aa99c877989212fe45704d014b67
erik-saltwell <169001579+erik-saltwell@users.noreply.github.com>
2025-01-28 20:26:47 -0800

    reset

--- forum_scraper.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Iterator
-from conversation_node import ConversationNode
-
-
-class ForumScraper(ABC):
-    @abstractmethod
-    def scrape(self) -> Iterator[ConversationNode]:
-        pass
