663fe3a2d2d31aa5a5e3845727eb1b68ff21b50b
erik-saltwell <erik.saltwell@gmail.com>
2025-02-06 11:01:04 -0800

    fixed formatting issue

--- tests/reddit_scrapers_test.py
+++ tests/reddit_scrapers_test.py
@@ -12,5 +12,5 @@ def test_double_reddit_scrape() -> None:
 
 def _run_reddit_Scrape(subreddit_name: str, expected_post_count: int) -> None:
     scraper: ForumScraper = _RedditScraper(subreddit_name)
-    nodes: list[ConversationNode] = list(scraper.Scrape(limit=expected_post_count)) 
+    nodes: list[ConversationNode] = list(scraper.Scrape(limit=expected_post_count))
     assert len(nodes) == expected_post_count, "Scrape should return at least 1 ConversationNode"
